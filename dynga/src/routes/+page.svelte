02163def07a4f39b87fbef5182e4573d7e5418dd
KatjaDeLuxembourg <kyoofluxembourg@gmail.com>
2022-08-17 00:24:06 +0100

    Initial Commit

---
+++ dynga/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<title>Websit Dyngās</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css.css">
+
+<meta name="description" content=".">
+<meta name="keywords" content="Dyngā">
+<body>
+        <h1 style="color:orange;text-align:center">Websit Dyngās</h1>
+        <h2 style="color:orange;text-align:center">Linkes:
+            <p style="color:orange;text-align:center">
+              <a href=".">Verdkrusom </a>
+              <br>
+              <a href=".">Trassekutues</a>
+              <br>
+              <a href="https://discord.gg/WsaUuqxFQG ">Discord</a>
+              <br>
+              <a href="https://www.youtube.com/channel/UCrHuTsLs9-pA4RIjtyzK_Eg">YouTube</a>
+              <br>
+            </p>
+            </h2>
+</body>
